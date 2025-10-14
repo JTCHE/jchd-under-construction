452f16ec236b3b182a4a06a957859b539c077605
JTCHE <john.chedeville@gmail.com>
2025-10-14 17:31:54 +0200

    feat: added little emoji in description

--- app/lib/copywriting/copywriting.tsx
+++ app/lib/copywriting/copywriting.tsx
@@ -2,7 +2,7 @@ const copywriting = {
   siteTitle: "JCHD.ME",
   siteDescription: "JCHD.ME is currently under construction. Check back soon for updates.",
   htmlSiteDescription:
-    '<a href="https://jchd.me">JCHD.ME</a> is currently under construction.<br>Check back soon for updates.',
+    '<a href="https://jchd.me">JCHD.ME</a> is currently under construction.<br>Check back soon for updates.🚧',
   progressNumber: 27,
 };
 
